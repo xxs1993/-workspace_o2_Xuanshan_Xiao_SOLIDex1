3b1285c92b291f0bc73b372d09f3e851430ddb1a
AlexYiMu <42689860+AlexYiMu@users.noreply.github.com>
2018-10-13 22:08:12 -0400

    Add files via upload

---
+++ src/service/chooseMovieByID.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface chooseMovieByID {
+	public int chooseMovie(int id);
+}
---
+++ src/service/previousMovie.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface previousMovie {
+	
+	
+	public void previous();
+
+}
